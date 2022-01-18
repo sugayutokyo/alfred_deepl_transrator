f656073ef208c154bd4cfb02bcbd81b1925cfd5b
sugayutokyo <33997655+elletech@users.noreply.github.com>
2022-01-18 23:11:42 +0900

    large type

---
+++ sourceToTargetLarge.js
@@ -0,0 +1,8 @@
+import alfy from 'alfy';
+import { translate } from './utility/translate.js';
+
+const keyword = alfy.input;
+const targetLang = process.env.TARGET_LANG || '';
+const sourceLang = process.env.SOURCE_LANG || '';
+
+await translate(targetLang, sourceLang, keyword, true);
--- utility/translate.js
+++ utility/translate.js
@@ -1,7 +1,12 @@
 import alfy from 'alfy';
 import { deeplTranslate } from './fetchDeeplTranslate.js';
 
-export const translate = async (targetLang, sourceLang, keyword) => {
+export const translate = async (
+  targetLang,
+  sourceLang,
+  keyword,
+  largeType = false,
+) => {
   // 最後の文字が;の場合のみ処理を走らせる
   if (keyword.slice(-1) === ';') {
     const keywordExceptLastChar = keyword.slice(0, -1);
@@ -13,11 +18,33 @@ export const translate = async (targetLang, sourceLang, keyword) => {
     );
 
     const items = translations.map(trans => {
-      return {
-        title: trans.text,
-        subtitle: trans.text,
-        arg: trans.text,
-      };
+      if (!largeType) {
+        return {
+          title: trans.text,
+          subtitle: trans.text,
+          arg: trans.text,
+        };
+      } else {
+        // let argLargeType = '--input--\n';
+        let argLargeType = '';
+        // argLargeType = argLargeType + keywordExceptLastChar + '\n';
+        // argLargeType = argLargeType + '\n--output--\n';
+
+        // 出力する文字列を30文字ごとに改行を加える
+        let arraySplitSyntax = [...trans.text];
+        arraySplitSyntax.map((char, index) => {
+          if (index != 0 && index % 25 === 0) {
+            argLargeType = argLargeType + '\n';
+          }
+          argLargeType = argLargeType + char;
+        });
+
+        return {
+          title: trans.text,
+          subtitle: trans.text,
+          arg: argLargeType,
+        };
+      }
     });
 
     alfy.output(items);
